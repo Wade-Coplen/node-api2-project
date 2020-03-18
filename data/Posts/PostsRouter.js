097a6c5aafb9ce77cec360e74a5cfcdcc23f9ae5
Wade-Coplen <wadecoplen@gmail.com>
2020-03-17 20:41:03 -0700

    IV- GET done

--- data/Posts/PostsRouter.js
+++ data/Posts/PostsRouter.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const Posts = require('./PostsModels');
 
@@ -7,18 +8,62 @@ const router = express.Router();
 //     res.status(200).send('yay! you did it!')
 // });
 
-router.get('/', (req, res) => {
-    Posts.find()
-    .then(posts => {
-        res.status(200).json(posts)
-    })
-    .catch(err => {
-        res.status(500).json({error: "The posts information could not be retrieved." })
-    })
-})     
+// router.get('/', (req, res) => {
+//     Posts.find()
+//     .then(posts => {
+//         res
+//         .status(200).json(posts)
+//     })
+//     .catch(err => {
+//         res.status(500)
+//         .json({error: "The posts information could not be retrieved." })
+//     })
+// })     
 
-router.get('/api/posts/:id', (req, res) => {
-    Posts.findById(id)
+// router.get('/:id', (req, res) => {
+//     Posts.findById(req.params.id)
+//     .then(item => {
+//         res.status(200).json(item);
+//     })
+//         if(req.params.id !== Posts) {
+              
+//             res.status(404).json({ message: "The post with the specified ID does not exist." })        
+//         } else {
+//             res.status(500).json({ error: "The post information could not be retrieved." })
+//         }
+//     })
+// router.get('/:id/comments', (req, res) => {
+//     Posts.findPostComments(req.params.post_id)
+//     .then(item => {
+//         res.status(200).json(item);
+//     })
+//     const param = req.params.post_id;
+//     if(param !== Posts) {
+//         res.status(404).json({ message: "The post with the specified ID does not exist." })
+//     } else {
+//         res.status(500).json({ error: "The comments information could not be retrieved." })
+//     }
+// })
+ 
+
+
+
+//POSTS=====================
+router.post('/', (req, res) => {
+    Posts.insert(req.body)
+    .then(item => {
+        res.status(201).json(item)
+    })    
+    .catch(err => {
+
+    
+    if(!req.body.title || !req.body.contents) {
+        res.status(400).json({errorMessage: "Please provide title and contents for the post." })
+        
+    } else {
+        res.status(500).json({error: "There was an error while saving the post to the database"})
+    }
+})
 })
 
 module.exports = router;
