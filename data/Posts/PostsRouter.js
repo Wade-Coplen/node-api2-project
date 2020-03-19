e61226c251b11f00ea185dd4e4250d4ed0be67a8
Wade-Coplen <wadecoplen@gmail.com>
2020-03-18 18:51:16 -0700

    V- one POST left

--- data/Posts/PostsRouter.js
+++ data/Posts/PostsRouter.js
@@ -8,17 +8,21 @@ const router = express.Router();
 //     res.status(200).send('yay! you did it!')
 // });
 
-// router.get('/', (req, res) => {
-//     Posts.find()
-//     .then(posts => {
-//         res
-//         .status(200).json(posts)
-//     })
-//     .catch(err => {
-//         res.status(500)
-//         .json({error: "The posts information could not be retrieved." })
-//     })
-// })     
+//WORKS
+
+router.get('/', (req, res) => {
+    Posts.find()
+    .then(posts => {
+        res
+        .status(200).json(posts)
+    })
+    .catch(err => {
+        res.status(500)
+        .json({error: "The posts information could not be retrieved." })
+    })
+})  
+
+//WORKS
 
 // router.get('/:id', (req, res) => {
 //     Posts.findById(req.params.id)
@@ -32,6 +36,35 @@ const router = express.Router();
 //             res.status(500).json({ error: "The post information could not be retrieved." })
 //         }
 //     })
+
+
+//WORKS
+// router.post('/', (req, res) => {
+//     Posts.insert(req.body)
+//     .then(item => {
+//         res.status(201).json(item)
+//     })    
+//     .catch(err => {    
+//     if(!req.body.title || !req.body.contents) {
+//         res.status(400).json({errorMessage: "Please provide title and contents for the post." })
+        
+//     } else {
+//         res.status(500).json({error: "There was an error while saving the post to the database"})
+//     }
+// })
+// })
+
+router.post('/api/posts/:id/comments', (req, res) => {
+    Posts.insertComment(req.body)
+    .then(item => {
+        res.status(201).json(item);
+    })
+    .catch(err => {
+        if(
+       res.status(404).json({something})
+    })
+})
+
 // router.get('/:id/comments', (req, res) => {
 //     Posts.findPostComments(req.params.post_id)
 //     .then(item => {
@@ -48,22 +81,6 @@ const router = express.Router();
 
 
 
-//POSTS=====================
-router.post('/', (req, res) => {
-    Posts.insert(req.body)
-    .then(item => {
-        res.status(201).json(item)
-    })    
-    .catch(err => {
 
-    
-    if(!req.body.title || !req.body.contents) {
-        res.status(400).json({errorMessage: "Please provide title and contents for the post." })
-        
-    } else {
-        res.status(500).json({error: "There was an error while saving the post to the database"})
-    }
-})
-})
 
 module.exports = router;
